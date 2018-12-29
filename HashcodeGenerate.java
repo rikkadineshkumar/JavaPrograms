a38d6027daf0b9e903e3e1640b6a72a32575dacd
R Dinesh Kumar <danialdinesh333@gmail.com>
2018-12-30 02:08:34 +0530

    adding programs

---
+++ HashcodeGenerate.java
@@ -0,0 +1,31 @@
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class HashcodeDemo1 {
+
+	public static String hashPassword(String password) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		byte[] b = md.digest();
+		StringBuffer sb = new StringBuffer();
+		for(byte b1 : b) {
+			sb.append(Integer.toHexString(b1 & 0xff).toString());	
+		}
+		return sb.toString();
+	}
+	public static void main(String[] args) {
+		
+		System.out.print("Enter the string::");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		try {
+			System.out.println("hashcode::9c9f1c65b1dc1f79498c9f9eb61e1a");
+			System.out.println("hashcode::"+HashcodeDemo1.hashPassword(s));
+		}catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
